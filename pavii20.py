ce519996522dd7b1f2cda1c21f35fb3d648bb8f9
paviprajansai <52271026+paviprajansai@users.noreply.github.com>
2019-08-02 14:52:36 +0530

    Create pavii20.py

---
+++ pavii20.py
@@ -0,0 +1,26 @@
+pl=input()
+a1=""
+k1=""
+for i in p1:
+    if i=="x":
+        k1="a1"
+        a1=a1+k1
+    elif i=="y":
+        k1="b1"
+        a1=a1+k1
+    elif i=="z":
+        k1="c1"
+        a1=a1+k1
+    elif i=="Y":
+        k1="B1"
+        a1=a1+k1
+    elif i=="Z":
+        k1="C1"
+        a1=a1+k1
+    elif i=="X":
+        k1=="A1"
+    else:
+        k1=ord(i)+3
+        a1=a1+str(chr(k1))
+	
+print(a1)
