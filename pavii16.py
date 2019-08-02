226177c86027ede7d819884f8aa6083c55e2a6b4
paviprajansai <52271026+paviprajansai@users.noreply.github.com>
2019-08-02 14:45:06 +0530

    Create pavii16.py

---
+++ pavii16.py
@@ -0,0 +1,12 @@
+n11=int(input())
+s11==input()
+t1=[]
+for i in s11:
+    t1.append(s11.count(i))
+for i in range(0,len(t1)):
+    a=min(t1)
+    if t1[i]==a:
+        print(s11[i])
+        break
+#i
+#s
